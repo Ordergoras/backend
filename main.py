456c8fba0293d63e6219ace4fa325de7573cf458
Felix Wagner <felixwagner99@googlemail.com>
2022-03-29 23:08:31 +0200

    add flask server

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from DatabaseIO import DatabaseIO
+from flask import Flask
+
+from api.OrdersApi import ordersApi
+from api.StorageApi import storageApi
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+app.register_blueprint(storageApi, url_prefix='/storage')
+app.register_blueprint(ordersApi, url_prefix='/orders')
 
 
-dbio = DatabaseIO()
-dbio.insertStorageData(('Krustenbraten', 70))
+@app.route('/')
+def home():
+    return 'Yeet'
+
+
+app.run()
