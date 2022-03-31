e3a9e8f5971707278b1164311ed4fce9b469b499
Felix Wagner <felixwagner99@googlemail.com>
2022-03-31 15:32:25 +0200

    add favicon WIP

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from api.OrdersApi import ordersApi
+from api.StaffApi import staffApi
 from api.StorageApi import storageApi
 
 app = Flask(__name__)
@@ -7,11 +9,17 @@ app.config['JSON_AS_ASCII'] = False
 
 app.register_blueprint(storageApi, url_prefix='/storage')
 app.register_blueprint(ordersApi, url_prefix='/orders')
+app.register_blueprint(staffApi, url_prefix='/staff')
 
 
 @app.route('/')
 def home():
     return 'Yeet'
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 
 app.run()
